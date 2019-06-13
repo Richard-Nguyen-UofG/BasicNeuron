ea550cfca983967e08a46014b9f3d4c600fc45eb
Richard Nguyen <richard_nguyen@live.ca>
2019-06-13 01:17:12 -0400

    readability and documentation improvements

--- BasicNeuron.py
+++ BasicNeuron.py
@@ -2,79 +2,81 @@
 # Richard Nguyen
 # 9 Dec 2018
 #
-# A neuron which learns given the age and wage of a person
-# whether or not they'll buy this product (example data)
-
-# TO DO:
-# Automatically remove (or option) to remove outliers
-# Input from a spreadsheet instead
-# Add labels for graphs
+# A neuron which learns, given the age and wage of a person
+# whether or not they'll buy a unnamed product (example data)
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Each tuple represents a person; their age and salary, 
+# Each tuple represents a person; their age, salary, 
 # and whether or not they would buy a eg. a house
-DataF=pd.read_excel("dataset.xlsx",sheet_name='sheet1')
-fPeopleList = DataF.values
+DataF = pd.read_excel("dataset.xlsx",sheet_name='sheet1')
+peopleList = DataF.values
 
-# A person which only age, and salary is known
-fPerson = [40.0, 80000.0]
+# A person for which only age and salary is known
+person = [40.0, 80000.0]
 
-iDATAAXISX = 105000
-iDATAAXISY = 105
+# Graph boundaries
+xAXIS = 105000
+yAXIS = 105
 
 fLEARNINGSPEED = 0.02
+iITERATIONS = 30000
 
-iITERATIONS = 50000
 
+# Plots the cost of 
 def makeCostGraph( fAllCosts ):
     plt.plot(fAllCosts)
 
-# Create a scatterplot of the fPeopleList
+
+# Plots the decision boundary and each person onto the scatterplot
 def make_graph():
-    plt.axis([0, iDATAAXISY, 0, iDATAAXISX])
+    plt.axis([0, yAXIS, 0, xAXIS])
     plt.grid()
     
-    # Display points which are most likely (non)purchase
+    # Displays the neural network's decision boundary
     for i in range(0,51):
         for e in range(0, 51):
             col = "yellow"
-            num = (i/fW1Coeff * w1*2) + (e*1000/fW2Coeff * w2*2) + b
+            num = (i/w1Coeff * w1*2) + (e*1000/w2Coeff * w2*2) + b
             ped = neuron(num)
+            
             if ped >= 0.5:
                 col = "green"
         
             plt.scatter(i*2,2*e*1000,c=col)
-    plt.plot([0,105],[0,105000], color = 'k', linestyle='-',linewidth=4)
     
-    # Plot each point
-    for i in range(len(fPeopleList)):
-        point = fPeopleList[i]
+    # Plots each person in the dataset
+    for i in range(len(peopleList)):
+        point = peopleList[i]
         colour = "red"
 
         if point[2] == 1:
             colour = "blue"
 
-        plt.scatter(point[0], point[1], c = colour) #c= colour
+        plt.scatter(point[0], point[1], c = colour) 
     
-    plt.scatter(fPerson[0], fPerson[1], c = "purple")
+    plt.scatter(person[0], person[1], c = "purple")
     
     plt.show()
 
-# Prevents super very numbers
+
+# Prevents hard to work with numbers
 def getridofe(x):
     if "e" in str(x):
         return 0
     else:
         return x
 
+# The sigmoid function
 def sigmoid(x):
     return getridofe( 1.0 / (1.0 + np.exp(-x)) )
 
+# The derivative of the sigmoid function
 def sigmoid_deriv(y):
     return sigmoid(y) * (1.0 - sigmoid(y))
+
 
 # Takes in the sum of inputs to the neuron and applies a function (sigmoid)
 def neuron(x):
@@ -82,29 +84,32 @@ def neuron(x):
 
 
 # Calculates coefficients to normalize data (to < 10)
-fW1Coeff = 1.0
-fW2Coeff = 1.0
-for i in fPeopleList:
-    while i[0]/fW1Coeff > 10:
-        fW1Coeff *= 10
-for e in fPeopleList:
-    while e[1]/fW2Coeff > 10:
-        fW2Coeff *= 10
+w1Coeff = 1.0
+w2Coeff = 1.0
+for i in peopleList:
+    while i[0]/w1Coeff > 10:
+        w1Coeff *= 10
+for e in peopleList:
+    while e[1]/w2Coeff > 10:
+        w2Coeff *= 10
 
-fCostList = []
+# Neuron related variables
+costList = []
 w1 = np.random.randn()
 w2 = np.random.randn()
 b = np.random.randn()
 
+
+# Begin training the neuron
 for i in range(iITERATIONS):
     # Chooses a random person to train with
-    rand = np.random.randint(len(fPeopleList))
-    point = fPeopleList[rand]
+    rand = np.random.randint(len(peopleList))
+    point = peopleList[rand]
     
-    fInputSum = (point[0]/fW1Coeff * w1) + (point[1]/fW2Coeff * w2) + b
+    inputSum = (point[0]/w1Coeff * w1) + (point[1]/w2Coeff * w2) + b
 
     # What the NN thinks the answer is
-    prediction = neuron(fInputSum)
+    prediction = neuron(inputSum)
     prediction = getridofe(prediction)
     #What the actual answer is
     target = point[2]
@@ -119,36 +124,38 @@ for i in range(iITERATIONS):
         w2 = np.random.randn()
         b = np.random.randn()
     
-    fCostList.append(cost)
+    costList.append(cost)
 
     cost_prediction_deriv = 2 * (prediction - target)
-    prediction_deriv = sigmoid_deriv(fInputSum)
+    prediction_deriv = sigmoid_deriv(inputSum)
     
-    # Derivatives of fInputSum with respect to ...
-    fInputSum_w1_deriv = point[0]/fW1Coeff
-    fInputSum_w2_deriv = point[1]/fW2Coeff
-    fInputSum_b_deriv = 1
+    # Derivatives of inputSum with respect to ...
+    inputSum_w1_deriv = point[0]/w1Coeff
+    inputSum_w2_deriv = point[1]/w2Coeff
+    inputSum_b_deriv = 1
     
     # Cost derivatives with respect to ...
     # Move every variable closer to f'(0) = 0 w/ respect to itself
-    cost_deriv_w1 = cost_prediction_deriv * prediction_deriv * fInputSum_w1_deriv
-    cost_deriv_w2 = cost_prediction_deriv * prediction_deriv * fInputSum_w2_deriv
-    cost_deriv_b = cost_prediction_deriv * prediction_deriv * fInputSum_b_deriv
+    cost_deriv_w1 = cost_prediction_deriv * prediction_deriv * inputSum_w1_deriv
+    cost_deriv_w2 = cost_prediction_deriv * prediction_deriv * inputSum_w2_deriv
+    cost_deriv_b = cost_prediction_deriv * prediction_deriv * inputSum_b_deriv
     
     # With each iteration each variable gets closer to f'(0)
     w1 = w1 - (fLEARNINGSPEED * cost_deriv_w1)
     w2 = w2 - (fLEARNINGSPEED * cost_deriv_w2)
     b = b - (fLEARNINGSPEED * cost_deriv_b)
 
-# Now that the NN is trained, predict the missing data
-fInputSum = (fPerson[0]/fW1Coeff * w1) + (fPerson[1]/fW2Coeff * w2) + b
-prediction = neuron(fInputSum)
 
-# Display whether or not cost is actually decreasing or not    
+# Now that the NN is trained, predict the missing data
+inputSum = (person[0]/w1Coeff * w1) + (person[1]/w2Coeff * w2) + b
+prediction = neuron(inputSum)
+
+
+# Display a graph regarding the cost (inaccuracy*) of the neuron   
 plt.figure(1)
 make_graph()
 plt.figure(2)
-makeCostGraph(fCostList)
+makeCostGraph(costList)
 
 print("Probability of purchase")
 print( round(prediction, 6) )
--- GenerateTestData.py
+++ GenerateTestData.py
@@ -1,62 +1,35 @@
 # GenerateTestData.py
 # Richard Nguyen
 # 15 Dec 2018
-# Generates data for the neuron to use
+# Generates example people data (age, salary, purchase unnamed item)
 
-#Import Libraries
+# Import Libraries
 import random
 from pandas import DataFrame
 
-# Work in Progress
-def takeParameters():
-    sCategory1 = "Age"
-    sCategory2 = "Wage"
-    iPurchase = 0 #0or1
-    
-    iPoints = 100
-    print(sCategory1 + sCategory2 + iPurchase + iPoints)
-
-# Generates whether or not this person is a buy, based off a probability
-def generateRandom(x):
-    point = random.randint(0,11)/10.0
-    if point < x:
-        point = 1.0
-    else:
-        point = 0.0
-    return point
-
-# Generate dataset
+# Generates dataset
 def generateData():
     iPoints = 1000
    
     personList = []
     
     for i in range(0, iPoints):
-        p1 = random.randint(1,101)
-        p2 = random.randint(1000,100001)
+        age = random.randint(1,100)
+        salary = random.randint(1000,100000)
         
-        if p2 >= p1*1000:
-            p3 = generateRandom(0.7)
-        elif p2 < p1*1000:
-            p3 = generateRandom(0.3)
+        # Determines function of data (decision boundary)
+        if salary > (age*400) + 35000:
+            purchase = 1.0
         else:
-            p3 = generateRandom(0.5)
-        #if p2 > 100*(p1-50)*(p1-50) + 5000:
-        #    p3 = 1.0
-        #else:
-        #    p3 = 0.0
-        
-        #if p1 > 45.0:
-        #    p3 = 1.0
-        #else:
-        #    p3 = 0.0
-        person = (p1, p2, p3)
+            purchase = 0.0
+
+        person = (age, salary, purchase)
         personList.append(person)
 
     return personList
 
 people = generateData()
-df = DataFrame(people, columns = ['Age', 'Wage', 'Purchase'])
+df = DataFrame(people, columns = ['Age', 'Salary', 'Purchase'])
 df.to_excel('dataset.xlsx', sheet_name='sheet1', index=False)
 print(df)
         
